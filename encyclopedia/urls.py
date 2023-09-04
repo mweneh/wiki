ff73f8610386971cbf5f7ce038ed87f0489cb482
David <davidongaroh@gmail.com>
2023-09-04 13:03:19 +0300

    search +query not redirecting

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.entry, name="entry"),
-    path("wiki/<str:title>", views.search, name="search"),
+    path("search", views.search, name="search"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from . import util
 
@@ -13,8 +13,13 @@ def entry(request, title):
     return render(request, "encyclopedia/entry.html", {
         "title": title, "content": content
     })
-def search(self, request, title):
-    content = util.get_entry(self)
-    return render(request, "encyclopedia/search.html",{
-        "title": title, "content": content
-    })
+def search(request):
+    query = request.GET.get('q', '')  # Get the query from the request's GET parameters
+    entries = util.list_entries()
+    matching_entries = [entry for entry in entries if query.lower() in entry.lower()]
+
+    if len(matching_entries) == 1:
+        # If there's an exact match, redirect to the entry page
+        return redirect('entry', title=matching_entries[0])
+    else:
+        return render(request, "encyclopedia/search.html", {"query":query, "entries":matching_entries})
