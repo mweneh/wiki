c540953482a8e6064c1df50941eb39e8c290c162
David <davidongaroh@gmail.com>
2023-09-06 12:09:55 +0300

    include the str:title line

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("wiki/<str:title>", views.entry, name="entry"),
     path("search", views.search, name="search"),
     path("add", views.add, name="add"),
-    path("edit", views.edit, name="edit"),
+    path("edit/<str:title>", views.edit, name="edit"),
 ]
