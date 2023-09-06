e5b059195f306aa947e1edba53c9b111d411c67c
David <davidongaroh@gmail.com>
2023-09-06 13:52:03 +0300

    create a random path

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("search", views.search, name="search"),
     path("add", views.add, name="add"),
     path("edit/<str:title>", views.edit, name="edit"),
+    path("random", views.random, name="random"),
 ]
